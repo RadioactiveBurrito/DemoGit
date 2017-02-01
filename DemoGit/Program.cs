8751df2072eb14870f25d5d30ab1fed7281fd295
Ludovic Bédard <ludovic.bedard@gmail.com>
2017-02-01 10:28:12 -0500

    oktamer

--- DemoGit/Program.cs
+++ DemoGit/Program.cs
@@ -10,6 +10,7 @@ namespace DemoGit
     {
         static void Main(string[] args)
         {
+            //Cette phrase est inutile.
             Console.WriteLine("Hello world!");
         }
     }
